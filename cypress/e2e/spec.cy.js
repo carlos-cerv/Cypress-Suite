03502429e181c8731b9eaed015fe21cf4e9cc128
Carlos Eduardo Cervantes Arteaga <89818143+carlos-cerv@users.noreply.github.com>
2023-04-06 18:52:22 -0600

    06/04/2023 New Cypress initial configuration

---
+++ cypress/e2e/spec.cy.js
@@ -0,0 +1,8 @@
+describe('template spec', () => {
+  it('passes', () => {
+    cy.visit('https://example.cypress.io')
+  })
+  it('passes', () => {
+    cy.visit('https://www.google.com.mx')
+  })
+})
